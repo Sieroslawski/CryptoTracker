fa7180965f79f99173c62fbb28449ae00253dc5f
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-07-01 15:05:59 -0700

    Fixed json deserialization issue, add css to dropdown

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,27 @@
 ﻿using CryptoTracker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CryptoTracker.Controllers
 {
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IHttpClientFactory _clientFactory;
-        public  IEnumerable<Trending> Trend { get; set; }
+        
         const string BASE_URL = "https://api.coingecko.com/api/v3/";
         public bool GetError { get; private set; }
+      
 
         public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
         {
@@ -27,6 +31,8 @@ namespace CryptoTracker.Controllers
 
         public async Task<IActionResult> Index()
         {
+            List<Coin> Trend = new List<Coin>();
+
             var message = new HttpRequestMessage();
             message.Method = HttpMethod.Get;
             message.RequestUri = new Uri($"{BASE_URL}search/trending");
@@ -37,14 +43,11 @@ namespace CryptoTracker.Controllers
 
             if (response.IsSuccessStatusCode)
             {
-                using var responseStream = await response.Content.ReadAsStreamAsync();
-                dynamic Trend = await JsonSerializer.DeserializeAsync<IEnumerable<Trending>>(responseStream);
-                return View(Trend);
-            } else
-            {
-                GetError = true;
-                Trend = Array.Empty<Trending>();
-            }
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var objResponse = JsonConvert.DeserializeObject<Trending>(responseContent);
+                Trend = objResponse.coins.ToList();
+            } 
+
             return View(Trend);
         }
 
