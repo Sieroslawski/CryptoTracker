538ba2efcc1e1f6f62294bf0f30abfeb53120c67
Sieroslawski <adrian.sieroslawski@gmail.com>
2022-08-22 16:10:24 -0700

    Create searchable coins

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace CryptoTracker.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IHttpClientFactory _clientFactory;
         
-        const string BASE_URL = "https://api.coingecko.com/api/v3/";
+        const string BASE_URL = "https://api.coingecko.com/api/v3/search";
         public bool GetError { get; private set; }
       
 
@@ -33,29 +33,55 @@ namespace CryptoTracker.Controllers
         public async Task<IActionResult> Index()
         {           
             try {
-
                 List<Coin> Trend = new List<Coin>();
+                
                 dynamic model = new ExpandoObject();
+
                 var message = new HttpRequestMessage();
                 message.Method = HttpMethod.Get;
-                message.RequestUri = new Uri($"{BASE_URL}search/trending");
+                message.RequestUri = new Uri($"{BASE_URL}/trending");
                 message.Headers.Add("Accept", "application/json");
-
+                              
                 var client = _clientFactory.CreateClient();
                 var response = await client.SendAsync(message);
-
+                
                 if (response.IsSuccessStatusCode)
                 {
                     var responseContent = await response.Content.ReadAsStringAsync();
                     var objResponse = JsonConvert.DeserializeObject<Trending>(responseContent);
                     model.Trend = objResponse.coins.ToList();
-                }
+                   
+                }                             
 
-                return View(model);
-        } catch (Exception ex)
+                return View(model);               
+            } catch (Exception ex)
             {
                 throw ex;
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string CoinSearch)
+        {
+            List<Coins> Search = new List<Coins>();
+            dynamic model = new ExpandoObject();
+
+            var searchMessage = new HttpRequestMessage();
+            searchMessage.Method = HttpMethod.Get;
+            searchMessage.RequestUri = new Uri($"{BASE_URL}?query={CoinSearch}");
+            searchMessage.Headers.Add("Accept", "application/json");
+
+            var clientSearch = _clientFactory.CreateClient();
+            var responseSearch = await clientSearch.SendAsync(searchMessage);
+
+            if (responseSearch.IsSuccessStatusCode)
+            {
+                var responseContent = await responseSearch.Content.ReadAsStringAsync();
+                var objResponse = JsonConvert.DeserializeObject<Search>(responseContent);
+                model.Search = objResponse.coins.ToList();
+            }
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Repositories/TransactionRepo.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.Repositories
+{
+    public class TransactionRepo
+    {
+        public List<Trending> GetTrend()
+        {
+            List<Trending> trending = new List<Trending>();
+            return trending;
+        }
+    }
+}
